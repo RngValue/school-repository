50e9747a50a887f0c91e4a206a7c0cfe4e272bbf
CursedCodMan <Bezmesiaca@seznam.cz>
2024-01-23 11:34:48 +0100

    aaaaaaaaaaaaaa

--- html/pc-komponenty/script.js
+++ html/pc-komponenty/script.js
@@ -1,10 +1,9 @@
 let produkty = localStorage.length;
-
 document.getElementById("produkticky").innerHTML = produkty;
 
 function pridej_produkt(produkt, cena) {
-  localStorage.setItem(produkt, cena);
-  produkty += 1;
-  document.getElementById("produkticky").innerHTML = produkty;
-  window.alert(`${produkt} byl přídán do košíku!`);
+  produkty ++
+  localStorage.setItem(produkt + " (" + produkty - 1 + ")", cena)
+  document.getElementById("produkticky").innerHTML = produkty - 1;
+  window.alert(`${produkt + " (" + produkty + ")"} byl přídán do košíku!`);
 }
--- html/pc-komponenty/script_cart.js
+++ html/pc-komponenty/script_cart.js
@@ -1,12 +1,11 @@
 let produkty = localStorage.length;
-
 document.getElementById("produkticky").innerHTML = produkty;
 
 const leDuck = document.getElementById("grapes");
 
 let celkem_cena = 0;
 
-for (let i = 0; i < produkty; i+=1) {
+for (let i = 0; i < produkty; i++) {
   leDuck.innerHTML += "<tr id=\"" + localStorage.key(i) + "\" >\n" + "<td>" + localStorage.key(i) + "</td>\n" + "<td>" + Number(localStorage.getItem(localStorage.key(i))).toLocaleString('de-DE') + " Kč</td>\n" + "<td>" + "<button onclick=\"localStorage.removeItem('" + localStorage.key(i) + "'); document.getElementById('" + localStorage.key(i) + "').remove(); window.alert('" + localStorage.key(i) + " byl odebrán z košíku!');" + "\" >odebrat</button>" + "</td>" + "<tr>";
   celkem_cena += +localStorage.getItem(localStorage.key(i));
 }
@@ -14,8 +13,8 @@ for (let i = 0; i < produkty; i+=1) {
 document.getElementById("celkem").innerHTML = Number(celkem_cena).toLocaleString('de-DE');
 
 function pridej_produkt(produkt, cena) {
-  localStorage.setItem(produkt, localStorage.getItem(produkt)+cena);
-  produkty += 1;
-  document.getElementById("produkticky").innerHTML = produkty;
-  window.alert(`${produkt} byl přídán do košíku!`);
+  produkty++
+  localStorage.setItem(produkt + " (" + produkty + ")", cena)
+  document.getElementById("produkticky").innerHTML = produkty - 1;
+  window.alert(`${produkt + " (" + produkty + ")"} byl přídán do košíku!`);
 }
