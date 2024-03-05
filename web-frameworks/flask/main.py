5bd33c91876d023ace759f9628aef0b13bbf65fb
CursedCodMan <Bezmesiaca@seznam.cz>
2024-03-05 11:35:39 +0100

    gsgsdsgdxg

--- web-frameworks/flask/main.py
+++ web-frameworks/flask/main.py
@@ -2,18 +2,12 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("main.html", css = url_for("static", filename="styles.css"), sample_text = "Hehe")
-
-@app.route("/blah", methods=['POST', 'GET'])
-def ah():
-    if request.method == "GET":
-        print("aaa")
-        return render_template("main.html", css = url_for("static", filename="stylesblah.css"), sample_text = "Lmao")
-    if request.method == "POST":
-        print("doing stuff with \"" + request.json['sheesh'] + "\"")
-        return request.json['sheesh'] + " :3"
+    number = 0
+    # if request.method == "GET":
+    #     number = request.form["erst_nummer"] + request.form["zweit_nummer"]
+    return render_template("main.html", css = url_for("static", filename="styles.css"), result = number)
 
 if __name__ == "__main__":
     app.run()
