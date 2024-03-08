a0c6ea01237427f2b9b4f3c87623806c859ac545
CursedCodMan <Bezmesiaca@seznam.cz>
2024-03-08 08:47:25 +0100

    kalkulátor

--- web-frameworks/flask/main.py
+++ web-frameworks/flask/main.py
@@ -2,12 +2,20 @@ from flask import *
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET"])
+@app.route("/", methods=["GET", "POST"])
 def index():
     number = 0
-    # if request.method == "GET":
-    #     number = request.form["erst_nummer"] + request.form["zweit_nummer"]
+    if request.args.get("erst_nummer") != None and request.args.get("zweit_nummer") != None:
+        number = float(request.args.get("erst_nummer")) + float(request.args.get("zweit_nummer"))
     return render_template("main.html", css = url_for("static", filename="styles.css"), result = number)
+
+@app.route("/bruw", methods=["GET", "POST"])
+def bruw():
+    match(request.method):
+        case "GET":
+            return "Bruh"
+        case "POST":
+            return "Bruw: {}".format(request.form["bruh"])
 
 if __name__ == "__main__":
     app.run()
