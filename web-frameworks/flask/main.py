e4d5fda22e2d8b65469e21d51fae9c2522b7321a
CursedCodMan <Bezmesiaca@seznam.cz>
2024-03-19 11:05:03 +0100

    damn

--- web-frameworks/flask/main.py
+++ web-frameworks/flask/main.py
@@ -5,8 +5,16 @@ app = Flask(__name__)
 @app.route("/", methods=["GET", "POST"])
 def index():
     number = 0
-    if request.args.get("erst_nummer") != None and request.args.get("zweit_nummer") != None:
-        number = float(request.args.get("erst_nummer")) + float(request.args.get("zweit_nummer"))
+    if request.args.get("erst_nummer") != None and request.args.get("zweit_nummer") != None and request.args.get("operation"):
+        match(request.args.get("operation")):
+            case "sub":
+                number = float(request.args.get("erst_nummer")) - float(request.args.get("zweit_nummer"))
+            case "mul":
+                number = float(request.args.get("erst_nummer")) * float(request.args.get("zweit_nummer"))
+            case "div":
+                number = float(request.args.get("erst_nummer")) / float(request.args.get("zweit_nummer"))
+            case _:
+                number = float(request.args.get("erst_nummer")) + float(request.args.get("zweit_nummer"))
     return render_template("main.html", css = url_for("static", filename="styles.css"), result = number)
 
 @app.route("/bruw", methods=["GET", "POST"])
